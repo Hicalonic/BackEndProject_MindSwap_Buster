fb84771d84d5e14402f7586c4d51207bb91f7095
Aguiar425 <aguiar425@gmail.com>
2023-03-20 16:04:20 +0000

    added packages

--- src/main/java/org/mindswap/Main.java
+++
@@ -1,7 +0,0 @@
-package org.mindswap;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/mindswap/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package org.mindswap.repository;
+
+import org.mindswap.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
