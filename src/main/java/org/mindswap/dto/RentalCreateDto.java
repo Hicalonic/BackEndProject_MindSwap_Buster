f8379d570b57df5a9b16bc0175a9a7349233855b
Aguiar425 <aguiar425@gmail.com>
2023-03-22 16:41:31 +0000

    createdDtos implemented

--- src/main/java/org/mindswap/dto/RentalCreateDto.java
+++ src/main/java/org/mindswap/dto/RentalCreateDto.java
@@ -1,6 +1,12 @@
 package org.mindswap.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.mindswap.model.Client;
+import org.mindswap.model.Movie;
+
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -8,4 +14,15 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RentalCreateDto {
+    @NotBlank(message = "Needs a start date")
+    private Date startDate;
+
+    @NotBlank(message = "Needs a delivery date")
+    private Date deliveryDate;
+
+    @NotBlank(message = "Needs a costumer")
+    private Client client;
+
+    @NotBlank(message = "Needs movies")
+    private List<Movie> movieList;
 }
--- src/main/java/org/mindswap/dto/WorkerCreateDto.java
+++ src/main/java/org/mindswap/dto/WorkerCreateDto.java
@@ -1,5 +1,8 @@
 package org.mindswap.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -8,4 +11,20 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class WorkerCreateDto {
+    @NotBlank(message = "Must have a  first name")
+    private String firstName;
+
+    @NotBlank(message = "Must have a last name")
+    private String lastName;
+
+    @NotNull(message = "Must have a store")
+    private Long storeId;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @NotBlank(message = "Must have a password")
+    private String password;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @NotBlank(message = "Must retype your password")
+    private String retypedPassword;
 }
