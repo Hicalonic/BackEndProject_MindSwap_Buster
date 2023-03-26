e17aa656583e7b4d9c917c2c98c2abfcbe92ee0a
hicalonic <hicalonic@gmail.com>
2023-03-26 20:39:36 +0100

    rental controller finished(maybe? hopefully?).

--- src/main/java/org/mindswap/model/User.java
+++ src/main/java/org/mindswap/model/User.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
